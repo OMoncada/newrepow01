3e30174192039499f385652ec50f6bc562f44b7b
O'Moncada <oscar.moncada702@gmail.com>
2025-03-07 23:51:55 -0300

    Primer commit - Agregando proyecto de la semana 1

--- server.js
+++ server.js
@@ -1,37 +1,51 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 /* ***********************
- * Routes
-View Engine and Templates
+ * Middleware
  *************************/
-app.use(require("./routes/static"))
-//Index route
-app.get("/", function(req, res){ 
-  res.render("index", {title: "Home"})
+app.use(session({
+  secret: process.env.SESSION_SECRET || "secret", // Define un secreto para la sesión
+  resave: false,
+  saveUninitialized: true
+}))
+
+// Express Messages Middleware
+app.use(flash())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
 })
 
+// Servir archivos estáticos correctamente
+app.use(express.static("public"))
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 /* ***********************
+ * Routes
+ *************************/
+app.get("/", function(req, res){ 
+  res.render("index", {title: "Home"})
+})
+
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
